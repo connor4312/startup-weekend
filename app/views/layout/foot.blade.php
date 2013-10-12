a47d383758e4de6e6e2770130647a9bc2773b189
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 14:49:06 -0500

    Analytics

--- app/views/layout/foot.blade.php
+++ app/views/layout/foot.blade.php
@@ -2,5 +2,15 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     {{ HTML::script('js/bootstrap.min.js') }}
     {{ HTML::script('js/jquery.min.js') }}
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44776360-1', 'mooody.co');
+  ga('send', 'pageview');
+
+</script>
   </body>
 </html>
