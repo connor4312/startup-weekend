cbb7d06889c5678f59a69426e81caa614854c745
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 17:49:09 -0500

    all boards

--- app/views/pages/allboards.blade.php
+++ app/views/pages/allboards.blade.php
@@ -2,17 +2,9 @@
 @include('layout.nav')
 <div class="row">
   <!-- ###################### LEFT NAVIGATION -->
-  <div class="col-lg-1">
-    <div id="left-nav">
-      <ul>
-        <li><a href="#"><img src="img/images.png"></a></li>
-        <hr>
-        <li><a href="#"><img src="img/color.png"></a></li>
-      </ul> 
-    </div>
-  </div>
+
   <!-- ####################### BOARD -->
-  <div class="col-lg-11">
+  <div class="col-lg-12">
     <div id="all-boards">
         <div class="row">
           <div class="col-lg-4">
