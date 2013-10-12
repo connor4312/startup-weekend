79747aa95dce11ca87a675f06a6b20c8d23a67a3
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 16:53:50 -0500

    navbar color

---
+++ app/views/pages/allboards.blade.php
@@ -0,0 +1,27 @@
+@include('layout.head')
+@include('layout.nav')
+<div class="row">
+  <!-- ###################### LEFT NAVIGATION -->
+  <div class="col-lg-1">
+    <div id="left-nav">
+      <ul>
+        <li><a href="#"><img src="img/images.png"></a></li>
+        <hr>
+        <li><a href="#"><img src="img/color.png"></a></li>
+      </ul> 
+    </div>
+  </div>
+  <!-- ####################### BOARD -->
+  <div class="col-lg-11">
+    <div id="all-boards">
+        <h1>Moody Logo</h1>
+        <h5>Created 10/12/13</h5>
+      </div>
+    <div id="board">
+
+      Images goes here
+    </div>
+  </div>
+
+</div>
+@include('layout.foot')
