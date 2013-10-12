977ba0f85a6364240124dbedc2dd7a0768519be5
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 15:30:36 -0500

    signup

--- app/views/pages/signup.blade.php
+++ app/views/pages/signup.blade.php
@@ -23,25 +23,4 @@
   </div>
 </div>
 
-<div class="row"><div id="explain">
-<div class="col-lg-4">
-
-<h3>Import images</h3>
-<img src="img/images.png">
-<p>Upload images for your computer, paste them from the web and arrange them however you want.</p>
-</div>
-<div class="col-lg-4">
-<h3>Create colors</h3>
-<img src="img/color.png">
-<p>Create color swatches right inside of the mooody app and arrange them on your board</p>
-</div>
-<div class="col-lg-4">
-<h3>Share your board</h3>
-<img src="img/share.png">
-<p>Share your board across the web with friends, colleagues or clients</p>
-</div>
-</div>
-
-</div>
-</div>
 @include('layout.foot')
