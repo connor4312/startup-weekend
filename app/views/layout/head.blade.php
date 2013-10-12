1fdda38caf66520240c145aebb5e473cdd5c5d46
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 13:52:23 -0500

    css

--- app/views/layout/head.blade.php
+++ app/views/layout/head.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>mooody</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     {{ HTML::style('css/style.css')}}
