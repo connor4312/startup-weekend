689acdb99707431d5b5e12e81b47082694ebfe19
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 13:54:16 -0500

    font

--- app/views/layout/head.blade.php
+++ app/views/layout/head.blade.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     {{ HTML::style('css/style.css')}}
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
 
   </head>
