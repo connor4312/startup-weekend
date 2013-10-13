dbe2bafc8038c3af4524bde61656b6fd1848c2f2
mynameiskyle <hello@mynameiskyle.com>
2013-10-13 10:05:15 -0500

    updated nav

--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -4,6 +4,7 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -13,14 +14,23 @@
       <a class="navbar-brand" href="#">mooody</a>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
+          @else
+          <ul class="nav navbar-nav">
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Boards <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Board #1</a></li>
+          <li><a href="#">Board #2</a></li>
+        </ul>
+      </li>
+      <li><a href="#"><i class="icon-plus-sign"></i>Create New Board</a></li>
+    </ul>
+    @endif
           <ul class="nav navbar-nav navbar-right">
             @if (Auth::guest())
             <!-- <li>{{ HTML::link('/account/login', 'Login') }}</li> -->
             <!-- <li>{{ HTML::link('/account/register', 'Register') }}</li> -->
-            @else
-            <li>{{ HTML::link('/board', 'My Boards') }}</li>
-            <li>{{ HTML::link('/board/new', 'New Board') }}</li>
-            @endif
+            
           </ul>
         </div>
       </div>
