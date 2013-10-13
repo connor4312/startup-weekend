64a76262759fe515ddbc2bafe403f6e05eb2136e
connor4312 <connor@Connors-MacBook-Air.local>
2013-10-13 10:54:05 -0500

    Styling fixes in navbar

--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -15,10 +15,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
           @if (Auth::check())
-          <ul class="nav navbar-nav">
+          <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Boards <b class="caret"></b></a>
               <ul class="dropdown-menu">
+                @foreach 
                 <li><a href="#">Board #1</a></li>
                 <li><a href="#">Board #2</a></li>
               </ul>
