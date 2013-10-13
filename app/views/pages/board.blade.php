b954c15333cc4cc67f8ee20245e90aa521a11e5a
connor4312 <connor@Connors-MacBook-Air.local>
2013-10-13 14:43:54 -0500

    Fixed modal name

--- app/views/pages/board.blade.php
+++ app/views/pages/board.blade.php
@@ -22,8 +22,8 @@
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
         </div><!-- /.modal -->
-        <a data-toggle="modal" href="#image" class="btn btn-default btn-add"><i class="icon-cloud-upload"></i> Upload Image</a>
-        <div class="modal fade" id="image" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <a data-toggle="modal" href="#imageupload" class="btn btn-default btn-add"><i class="icon-cloud-upload"></i> Upload Image</a>
+        <div class="modal fade" id="imageupload" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
