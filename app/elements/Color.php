31426b572144d67baffab02ac03996c8f2fe7fc7
connor4312 <connor@Connors-MacBook-Air.local>
2013-10-13 10:39:44 -0500

    Updated color for hex length

--- app/elements/Color.php
+++ app/elements/Color.php
@@ -2,7 +2,7 @@
 
 class Color {
 	public $fields = array(
-		'color' => 'required|between:5,12',
+		'color' => 'required|between:3,8',
 		'index' => 'required|numeric',
 		'x' => 'required|numeric',
 		'y' => 'required|numeric',
