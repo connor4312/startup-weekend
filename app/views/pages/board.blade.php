6eabc208495ce99d0e4e633b7f61d37b6e983042
mynameiskyle <hello@mynameiskyle.com>
2013-10-13 10:46:03 -0500

    Added a test modal and test button

--- app/views/pages/board.blade.php
+++ app/views/pages/board.blade.php
@@ -4,11 +4,27 @@
   <!-- ###################### LEFT NAVIGATION -->
   
     <div id="left-nav" class="pull-left">
-      <div class="addimage">
-        <div class="addimage-inside">
-          <i class="icon-picture"> Paste image URL</i>
-        </div>
+      
+          <a data-toggle="modal" href="#image" class="btn btn-default btn-add"><i class="icon-picture"> Paste image URL</i></a>
+        
+
+      <div class="modal fade" id="image" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Paste an image URL</h4>
       </div>
+      <div class="modal-body">
+        <input type="text" class="form-control" placeholder="Text input">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Add Image to Board</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
       <div class="addimage">
         <div class="addimage-inside">
@@ -42,6 +58,7 @@
   <a class="btn btn-default" href="#"><i class="icon-chevron-up"></i> Bring Forward</a>
   <a class="btn btn-default" href="#"><i class="icon-chevron-down"></i> Send Backwards</a>
 </div>
+<a class="btn btn-default"href="#"><i class="icon-share-alt"></i> Share your board</a>
   </div>      
       <div id="canvas">
   
