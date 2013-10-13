d14ef1ed4f842774c1d95780c94076849f149f35
mynameiskyle <hello@mynameiskyle.com>
2013-10-12 20:50:55 -0500

    board stuff

--- app/views/pages/board.blade.php
+++ app/views/pages/board.blade.php
@@ -4,33 +4,33 @@
 <div class="row">
   <!-- ###################### LEFT NAVIGATION -->
   
-    <div id="left-nav">
+    <div id="left-nav pull-left">
       <div class="addimage">
         <div class="addimage-inside">
-          <img src="img/image.png">
+          <img src="{{ URL::to('img/image.png') }}">
           Upload or Paste Images
         </div>
       </div>
 
       <div class="addimage">
         <div class="addimage-inside">
-          <img src="img/color.png">
+          <img src="{{ URL::to('img/color.png') }}">
           Create a color swatch
         </div>
       </div>
     </div>
   
   <!-- ####################### BOARD -->
+  <div id="board" class="pull-left">
+      <div id="board-title pull-left">
+          <h1>Moody Logo</h1>
+          <h5>Created 10/12/13</h5>
+        </div>
+      <div id="canvas">
   
-    <div id="board-title">
-        <h1>Moody Logo</h1>
-        <h5>Created 10/12/13</h5>
+        Images goes here
       </div>
-    <div id="board">
-
-      Images goes here
     </div>
-  
 
 </div>
 </div>
