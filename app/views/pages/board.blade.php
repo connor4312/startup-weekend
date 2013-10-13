db49b22ea32c5fef88e9ebeddffa188eb10e418f
mynameiskyle <hello@mynameiskyle.com>
2013-10-13 11:02:35 -0500

    new board buttons

--- app/views/pages/board.blade.php
+++ app/views/pages/board.blade.php
@@ -26,24 +26,70 @@
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
-      <div class="addimage">
-        <div class="addimage-inside">
-          <i class="icon-tint"> Create a color swatch</i>
-        </div>
-      </div>
+       <a data-toggle="modal" href="#image" class="btn btn-default btn-add"><i class="icon-ting"> Create a color swatch</i></a>
+        
 
-      <div class="addimage">
-        <div class="addimage-inside">
-          <i class="icon-dribbble"> Paste dribbble URL</i>
-        </div>
+      <div class="modal fade" id="image" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Paste an image URL</h4>
       </div>
+      <div class="modal-body">
+        <input type="text" class="form-control" placeholder="Text input">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Add Image to Board</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
-      <div class="addimage">
-        <div class="addimage-inside">
-          <i class="icon-pinterest"> Paste Pinterest URL</i>
-        </div>
+       <a data-toggle="modal" href="#dribbble" class="btn btn-default btn-add"><i class="icon-dribbble"> Paste Dribbble URL</i></a>
+        
+
+      <div class="modal fade" id="dribbble" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Paste a Dribbble URL</h4>
       </div>
-    </div>
+      <div class="modal-body">
+        <p>Paste a Dribbble bucket URL to add all the images from the bucket to your board</p>
+        <input type="text" class="form-control" placeholder="Text input">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Add Images to Board</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+       <a data-toggle="modal" href="#pinterest" class="btn btn-default btn-add"><i class="icon-pinterest"> Paste a Pinterest URL</i></a>
+        
+
+      <div class="modal fade" id="pinterest" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Paste a Pinterest board URL</h4>
+      </div>
+      <div class="modal-body">
+        <p>Paste the URL of a Pinterest board to add the images to your mooody board</p>
+        <input type="text" class="form-control" placeholder="Text input">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Add Images to Board</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
   
   <!-- ####################### BOARD -->
   <div id="board" class="pull-left">
